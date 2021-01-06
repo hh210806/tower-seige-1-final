12ff76b7733ae4bb1870c28b0e3b096292fba6bb
hh210806 <73598433+hh210806@users.noreply.github.com>
2021-01-06 22:21:01 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,22 @@
+class Block  {
+  constructor(x,y,width,height)  {
+      var options={
+          isStatic:false
+      }
+      this.width = width
+      this.height = height
+      this.body = Bodies.rectangle(x,y,width,height,options)
+      
+
+      World.add(world,this.body)
+
+  }
+  display()  {
+      rectMode(CENTER)
+    
+      fill(129,255,91)
+      strokeWeight(2);
+      rect(this.body.position.x,this.body.position.y,this.width,this.height)
+
+  }
+}
